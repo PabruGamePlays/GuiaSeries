86a1bd2e6ba66ea56a01f669a9e7cee33d86f45b
Pablo Miguel <pablo2003miguel0903@gmail.com>
2023-11-29 00:17:44 -0300

    Criação do projeto

---
+++ app/src/main/java/com/example/guiaseries/ListaGuia.java
@@ -0,0 +1,13 @@
+package com.example.guiaseries;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaGuia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_guia);
+    }
+}
---
+++ app/src/main/java/com/example/guiaseries/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.guiaseries;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button botao_login;
+    Button botao_cadastro;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botao_login = findViewById(R.id.btn_fazer_login);
+        botao_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ii = new Intent(getApplicationContext(), Login.class);
+                startActivity(ii);
+            }
+        });
+
+        botao_cadastro = findViewById(R.id.btn_fazer_cadastro);
+        botao_cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ii = new Intent(getApplicationContext(), Cadastro.class);
+                startActivity(ii);
+            }
+        });
+    }
+}
